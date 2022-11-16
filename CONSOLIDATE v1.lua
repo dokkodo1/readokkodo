5c7d79eaeec2af6c5c2797f161bf6bf2e9dedf1a
dokkodo <89461793+dokkodo1@users.noreply.github.com>
2022-11-16 11:29:53 +0200

    some edit was made for mork to see

--- CONSOLIDATE v1.lua
+++ CONSOLIDATE v1.lua
@@ -3,55 +3,55 @@
 local r = reaper
 
 function Msg(param)
-    reaper.ClearConsole()
-    reaper.ShowConsoleMsg(tostring(param))
+    r.ClearConsole()
+    r.ShowConsoleMsg(tostring(param))
 end
 
 function ProjTabNext()
-    reaper.Main_OnCommand(40861, 0)
+    r.Main_OnCommand(40861, 0)
 end
 
 function ProjTabPrev()
-    reaper.Main_OnCommand(40862, 0)
+    r.Main_OnCommand(40862, 0)
 end
 
 function TSItems()
-    reaper.Main_OnCommand(40290, 0)
+    r.Main_OnCommand(40290, 0)
 end
 
 function CreateRegion()
-    reaper.Main_OnCommand(40174, 0)
+    r.Main_OnCommand(40174, 0)
 end
 
 function Copy()
-    reaper.Main_OnCommand(41383, 0)
+    r.Main_OnCommand(41383, 0)
 end
 
 function Paste()
-    reaper.Main_OnCommand(42398, 0)
+    r.Main_OnCommand(42398, 0)
 end
 
 function Len_TS()
-    local TS_Start, TS_End = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
+    local TS_Start, TS_End = r.GetSet_LoopTimeRange(false, false, 0, 0, false)
     return TS_End - TS_Start
 end
 
 --[[
             USEFUL STUFF, RIGHT?
-reaper.GetSet_LoopTimeRange2()
-reaper.GetSet_LoopTimeRange()
-reaper.EnumProjects() -- 
-reaper.SelectAllMediaItems()
+r.GetSet_LoopTimeRange2()
+r.GetSet_LoopTimeRange()
+r.EnumProjects() -- 
+r.SelectAllMediaItems()
 --]]
 --[[
-reaper.SelectAllMediaItems(0)
+r.SelectAllMediaItems(0)
 TSItem()
-local Start_Length, End_Length = reaper.GetSet_LoopTimeRange(true, false, 0, 0, false)
+local Start_Length, End_Length = r.GetSet_LoopTimeRange(true, false, 0, 0, false)
 --]]
 
 function NumProjects()
     local Projects = 0
-    while reaper.EnumProjects(Projects, "") ~= nil do
+    while r.EnumProjects(Projects, "") ~= nil do
         Projects = Projects + 1
     end
     return Projects  
@@ -64,3 +64,6 @@ function GenerateRegions()
         CreateRegion()
     end
 end
+
+--local edit
+-- some edit
